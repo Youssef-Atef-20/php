73e9e1f8bd8ace2295f620ed2473f0b85ff9cf95
Youssef Atef <hh3928850@gmail.com>
2026-04-29 12:10:09 +0300

    d

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="navStyle.css">
-    <title>test</title>
+    <title>PHP First Project</title>
 </head>
 
 
