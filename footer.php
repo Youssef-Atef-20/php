cdc663dc9290c8486606c5fc86ad2158b214c41d
Youssef Atef <hh3928850@gmail.com>
2026-04-29 11:47:44 +0300

    d

---
+++ footer.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footerStyle.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <footer>
+        <a href="php.php">home</a>
+        <a href="index.html">about</a>
+        <a href="index.html">contact</a>
+        <a href="index.html">log in</a>
+        <a href="index.html">sign up</a>
+    </footer>
+
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="navStyle.css">
+    <title>test</title>
+</head>
+
+
+<body>
+
+
+    <?php
+    include("nav.php");
+    ?>
+
+    <main>
+        <div>klfjsdljflksdjf</div>
+        <div>asdasd</div>
+        <div>hghfg</div>
+        <div>tyutyut</div>
+        <div>qweqwe</div>
+    </main>
+
+
+    <?php
+    include("footer.php");
+    ?>
+
+
+
+</body>
+
+</html>
